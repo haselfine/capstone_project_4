3eda477d54f710ebbc60147f677cb07cf218282c
Ben <benhaseltine@gmail.com>
2020-10-16 01:14:31 -0500

    added function call_api_covers() to allow search for game's cover by game's id

---
+++ view/image.py
@@ -0,0 +1,20 @@
+import logging
+import requests
+import os
+
+logging.basicConfig(filename='debug.log', level=logging.DEBUG, format=f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+client = os.environ.get('CLIENT_ID')
+api_auth = os.environ.get('AUTHORIZATION')
+
+def call_api_covers(game_id):
+    url = 'https://api.igdb.com/v4/covers'
+    header_dict = {'Client-ID': client, 'Authorization': api_auth}
+    search_data = f'fields url; where game = {game_id};'
+
+    try:
+        api_request = requests.post(url, data= search_data, headers=header_dict)
+        cover = api_request.json()
+        create_image_link(cover)
+    except Exception as e:
+        logging.error(f'Something went wrong when calling the API: {e}')
+        return None
