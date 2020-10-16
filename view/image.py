a5d7fd072625321b6f84424c89e89d7620e8dbc4
Ben <benhaseltine@gmail.com>
2020-10-16 01:20:04 -0500

    add create_image_link function to change json response for url into something usable

--- view/image.py
+++ view/image.py
@@ -1,20 +1,27 @@
-import logging
-import requests
-import os
+import logging
+import requests
+import os
 
-logging.basicConfig(filename='debug.log', level=logging.DEBUG, format=f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-client = os.environ.get('CLIENT_ID')
-api_auth = os.environ.get('AUTHORIZATION')
+logging.basicConfig(filename='debug.log', level=logging.DEBUG, format=f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+client = os.environ.get('CLIENT_ID')
+api_auth = os.environ.get('AUTHORIZATION')
 
-def call_api_covers(game_id):
-    url = 'https://api.igdb.com/v4/covers'
-    header_dict = {'Client-ID': client, 'Authorization': api_auth}
-    search_data = f'fields url; where game = {game_id};'
+def call_api_covers(game_id):
+    url = 'https://api.igdb.com/v4/covers'
+    header_dict = {'Client-ID': client, 'Authorization': api_auth}
+    search_data = f'fields url; where game = {game_id};'
 
-    try:
-        api_request = requests.post(url, data= search_data, headers=header_dict)
-        cover = api_request.json()
-        create_image_link(cover)
-    except Exception as e:
-        logging.error(f'Something went wrong when calling the API: {e}')
-        return None
+    try:
+        api_request = requests.post(url, data=search_data, headers=header_dict)
+        cover = api_request.json()
+        create_image_link(cover)
+    except Exception as e:
+        logging.error(f'Something went wrong when calling the API: {e}')
+        return None
+
+def create_image_link(cover):
+    base_url = cover.url.replace('t_thumb', 't_720p')
+    final_url = base_url[:2]
+    print(final_url)
+
+call_api_covers(1000)
