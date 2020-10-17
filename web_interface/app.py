3c1edfd2a741ec3663c7f5d3171ff7cedfe1b7f5
cjdufault <dufaultconnor@gmail.com>
2020-10-17 10:02:37 -0500

    remove sample data

--- web_interface/app.py
+++ web_interface/app.py
@@ -18,17 +18,6 @@ class SearchResult:
     game_title: str
     game_id: int
 
-# TODO: Add final templates and replace all references to temp file
-# TODO: get real results from IGDB API and integrate w/ Flask
-sample_results = [SearchResult(game_title='Halo: Combat Evolved', game_id=740), 
-                  SearchResult(game_title='Halo 2', game_id=986), 
-                  SearchResult(game_title='Halo 3', game_id=987), 
-                  SearchResult(game_title='Halo: Reach', game_id=990),
-                  SearchResult(game_title='Halo 4', game_id=991), 
-                  SearchResult(game_title='Halo 5', game_id=6803), 
-                  SearchResult(game_title='Halo: Infinite', game_id=103281)]
-sample_image_url = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.NlLee0YLEeATwq6qh8HjjQHaLK%26pid%3DApi&f=1'
-
 # starts the web server
 def start():
     app.run()
