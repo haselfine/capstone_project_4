f102f513df35a232375b479c8da8b6e029a4c0c7
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-10-19 12:48:45 -0500

    added funtion to get game data

--- view/main_program.py
+++ view/main_program.py
@@ -12,6 +12,9 @@ def main():
     #TODO connect to APIs
     #TODO search games in Steam, Twitch.tv
     #TODO store game info
+    # get_game_urls(990)
+    # get_game_info(990)
+    
     pass
 
 
@@ -56,7 +59,7 @@ def get_game_urls(game_id):
 
     except Exception as e:
         return None
-        logging.error(f'No response recieved '+ e)
+        logging.error(f'No response recieved ' + e)
 
     return url_list
     
@@ -65,9 +68,48 @@ def get_game_info(game_id):
     client_id = os.environ.get('CLIENT_ID')
     auth = os.environ.get('AUTHORIZATION')
 
-    url = 'https://api.igdb.com/v4/websites'
+    url = 'https://api.igdb.com/v4/games'
     header_data = {'Client-ID': client_id, 'Authorization': auth}
-    body_data = f'fields rating, summary, websites; where game = {game_id};'
+    body_data = f'fields name, rating, summary, platforms, first_release_date, cover; where id = {game_id};'
+    
+    try:
+
+        res = requests.post(url, data=body_data, headers = header_data)
+        game_info = res.json()
+    except Exception as e:
+        return None
+        logging.error(f'No response recieved ' + e)
+
+    return game_info
+
+
+    def get_platforms(platforms_list):
+
+    client_id = os.environ.get('CLIENT_ID')
+    auth = os.environ.get('AUTHORIZATION')
+
+    platforms_string = ''
+
+    for platform in platfroms_list:
+
+        if i != len(platforms_list)-1:
+
+            platforms_string = platforms_string + platform + ' |'
+
+        else:
+
+            platforms_string = platforms_string + platform + ';'
+
+
+    url = 'https://api.igdb.com/v4/platforms'
+    header_data = {'Client-ID': client_id, 'Authorization': auth}
+    body_data = f'fields name,platform_logo; where id = {platforms_string}'
+    
+    res = requests.post(url, data = body_data, headers = header_data)
+    platform_data = res.json()
+
+    pprint(platform_data)
+
 
 if __name__ == '__main__':
     main()
