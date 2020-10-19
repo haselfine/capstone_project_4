fa633c8ad022f40ce257152438ff901dc4e7823a
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-10-19 12:55:09 -0500

    added funtion to get platform data

--- view/main_program.py
+++ view/main_program.py
@@ -14,7 +14,7 @@ def main():
     #TODO store game info
     # get_game_urls(990)
     # get_game_info(990)
-    
+    get_platforms([160,203])
     pass
 
 
@@ -83,30 +83,32 @@ def get_game_info(game_id):
     return game_info
 
 
-    def get_platforms(platforms_list):
+def get_platforms(platforms_list):
 
     client_id = os.environ.get('CLIENT_ID')
     auth = os.environ.get('AUTHORIZATION')
 
     platforms_string = ''
 
-    for platform in platfroms_list:
+    for i,platform in enumerate(platforms_list):
 
         if i != len(platforms_list)-1:
 
-            platforms_string = platforms_string + platform + ' |'
+            platforms_string = f'{platforms_string} id = {platform} |'
 
         else:
 
-            platforms_string = platforms_string + platform + ';'
+            platforms_string = f'{platforms_string} id = {platform};'
 
 
     url = 'https://api.igdb.com/v4/platforms'
     header_data = {'Client-ID': client_id, 'Authorization': auth}
-    body_data = f'fields name,platform_logo; where id = {platforms_string}'
+    body_data = f'fields name,platform_logo; where {platforms_string}'
     
-    res = requests.post(url, data = body_data, headers = header_data)
-    platform_data = res.json()
+    try:
+        res = requests.post(url, data = body_data, headers = header_data)
+        platform_data = res.json()
+    except:
 
     pprint(platform_data)
 
