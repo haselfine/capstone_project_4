75bedd65041004e0e71f7dbfc538a0525948da9d
cjdufault <dufaultconnor@gmail.com>
2020-10-21 14:56:18 -0500

    add bookmarks page and support for viewing bookmarked games

--- web_interface/app.py
+++ web_interface/app.py
@@ -4,10 +4,11 @@ Renders and displays templates from the templates directory
 Import this file and call app.start() to start web server
 """
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from dataclasses import dataclass
 from project_4.view.main_program import *
 from project_4.view.image import *
+from project_4.viewmodel.viewmodel import *
 
 app = Flask(__name__)
 client = os.environ.get('CLIENT_ID')
@@ -17,14 +18,17 @@ api_auth = os.environ.get('AUTHORIZATION')
 class SearchResult:
     game_title: str
     game_id: int
+    
 
 # starts the web server
 def start():
     app.run()
+    
 
 @app.route('/') # GET requests
 def home():
     return render_template('home.html')
+
 
 @app.route('/', methods=['POST'])
 def search_results():
@@ -35,6 +39,7 @@ def search_results():
         return render_template('home.html', search_term=search_term, list_heading='Search Results:', search_results=results)
     else:
         return render_template('home.html', list_heading='No Results Found')
+    
 
 @app.route('/game/<game_title>&<game_id>') # TODO: get title from request to API, not by including in url
 def game(game_title, game_id):
@@ -46,6 +51,27 @@ def game(game_title, game_id):
         poster_url = None
         
     return render_template('game.html', game_title=game_title, image_url=poster_url)
+
+
+@app.route('/bookmarks')
+def bookmarks():
+    bookmarked_games = get_all_games()[0]
+    
+    if bookmarked_games is not None and len(bookmarked_games) > 0:
+        return render_template('bookmarks.html', list_heading='All Bookmarks:', bookmarks='bookmarked_games')
+    else:
+        return render_template('bookmarks.html', list_heading='No Bookmarks (yet!)')
+
+
+@app.route('/bookmarked_game/<game_title>')
+def bookmarked_game(game_title):
+    game = find_game(game_title)[0]
+    
+    if game is not None:
+        return render_template('game.html', game_title=game.title, image_url=game.image_url)
+    else:
+        # redirect to home page if not found
+        redirect('/')
 
 
 def search_for_game(search_term):
