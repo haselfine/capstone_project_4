78fc566cca7bf92fb4bdc6e41e28f4533d3ac163
Ben <benhaseltine@gmail.com>
2020-10-20 22:12:38 -0500

    add make_it_small(), comments

--- view/image.py
+++ view/image.py
@@ -6,6 +6,8 @@ logging.basicConfig(filename='debug.log', level=logging.DEBUG, format=f'%(asctim
 client = os.environ.get('CLIENT_ID')
 api_auth = os.environ.get('AUTHORIZATION')
 
+cloudinary_id = os.environ.get('CLOUD_ID')
+
 def call_api_covers(game_id):
     url = 'https://api.igdb.com/v4/covers'
     header_dict = {'Client-ID': client, 'Authorization': api_auth}
@@ -23,7 +25,7 @@ def call_api_covers(game_id):
                 return None, error
             
         except Exception as e:
-            logging.error(f'Something went wrong when calling the API: {e}')
+            logging.debug(f'Something went wrong when calling the API: {e}')
             error = ('Error', data)
             return None, error
     
@@ -32,6 +34,14 @@ def call_api_covers(game_id):
         return None, error
 
 def create_image_link(data):
-    base_url = data[0]['url'].replace('t_thumb', 't_720p')
-    final_url = base_url[2:]
-    return final_url
+    base_url = data[0]['url'].replace('t_thumb', 't_720p') #change from thumbnail to 720p image
+    final_igdb_url = base_url[2:] #remove "//" from beginning
+    return make_it_small(final_igdb_url)
+
+def make_it_small(igdb_url): #cloudinary uses the igdb url to resize the image... We can use this 25,000 times for free. Limit game results. And use cache
+    if len(cloudinary_id > 0): #make sure it's the correct id
+        cloudinary_url = f'https://res.cloudinary.com/{cloudinary_id}/image/fetch/w_150,f_auto/https://'
+        return cloudinary_url + igdb_url
+    else:
+        error = ('Error', 'Need cloudinary ID')
+        return None, error
