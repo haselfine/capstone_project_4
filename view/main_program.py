186e7331288d4da346d96af3a762226dc2933d68
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-10-22 12:02:51 -0500

    added a new pyfile

--- view/main_program.py
+++ view/main_program.py
@@ -16,51 +16,6 @@ def main():
     pass
 
 
-#takes the game name input and returns a list of games with that name, and their id's.
-#If there was an error encouterd it will return None instead, if no games were found it will return an empty list
-def search_game_request(game_name):
-
-
-    client_id = os.environ.get('CLIENT_ID')
-    auth = os.environ.get('AUTHORIZATION')
-    
-    url = 'https://api.igdb.com/v4/search'
-    header_data = {'Client-ID': client_id, 'Authorization': auth}
-    body_data = f'search "{game_name}"; fields name;'
-
-    try:
-        res = requests.post(url, data = body_data, headers = header_data)
-        game_list = res.json()
-
-    except Exception as e:
-
-        error = 'An error occurred: ' + e
-        logging.error(error)
-        return None, error
-
-    return game_list
-
-    
-# funtion that takes the game id, and returns a list containing one dictionary that includes
-# all the data 
-def get_game_info(game_id):
-
-    client_id = os.environ.get('CLIENT_ID')
-    auth = os.environ.get('AUTHORIZATION')
-
-    url = 'https://api.igdb.com/v4/games'
-    header_data = {'Client-ID': client_id, 'Authorization': auth}
-    body_data = f'fields name, rating, platforms.name, summary, first_release_date, cover.url, websites.url; where id = {game_id};'
-    
-    try:
-
-        res = requests.post(url, data=body_data, headers = header_data)
-        game_info = res.json()
-    except Exception as e:
-        return None
-        logging.error(f'No response recieved ' + e)
-
-    return game_info
 
 
 
