5b0962b066aec49bf302c05b34fb55b7466b0cb5
cjdufault <dufaultconnor@gmail.com>
2020-10-28 10:03:18 -0500

    remove "project_4" from imports

--- view/main_program.py
+++ view/main_program.py
@@ -1,6 +1,6 @@
 import logging
 from pprint import pprint
-import project_4.web_interface.app as app
+import .web_interface.app as app
 
 logging.basicConfig(filename='debug.log', level=logging.DEBUG, format=f'%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
